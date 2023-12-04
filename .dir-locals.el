da5de1136a3937ca5d160098eacd749a4e4d90c4
pillowtrucker <githubino@janitor.chat>
2023-12-04 23:03:40 +0100

    play around with the setup

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((projectile-project-compilation-cmd . "cargo build -j8"
+	 (compile-command . "cargo build -j9")
+         (projectile-project-run-cmd . "cargo run")
+	 )
+      )
+ )
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,36 @@
 // Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
-use bevy::prelude::*;
-
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{PresentMode, WindowMode, WindowTheme},
+};
+use space_editor::{simple_editor_setup, SpaceEditorPlugin};
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "I am a window!".into(),
+                    decorations: false,
+                    mode: WindowMode::Windowed,
+                    present_mode: PresentMode::AutoVsync,
+                    // Tells wasm to resize the window according to the available canvas
+                    fit_canvas_to_parent: true,
+                    // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                    prevent_default_event_handling: false,
+                    window_theme: Some(WindowTheme::Dark),
+                    ..default()
+                }),
+                ..default()
+            }),
+            //            LogDiagnosticsPlugin::default(),
+            FrameTimeDiagnosticsPlugin,
+            SpaceEditorPlugin::default(),
+        ))
         .add_systems(Startup, setup)
+        .add_systems(Startup, simple_editor_setup)
         .run();
 }
 
