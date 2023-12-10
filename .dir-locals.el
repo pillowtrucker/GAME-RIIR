7eec65282c021855ace25e8c25ecacf572e88c1b
pillowtrucker <githubino@janitor.chat>
2023-12-10 15:29:51 +0100

    add 2d stuff

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
-((nil . ((projectile-project-compilation-cmd . "cargo build -j8"
-	 (compile-command . "cargo build -j9")
+((nil . ((projectile-project-compilation-cmd . "cargo build -j8")
+	 (compile-command . "cargo build -j8")
          (projectile-project-run-cmd . "cargo run")
 	 )
       )
  )
+
--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,38 @@
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
 use bevy::{
-    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    diagnostic::FrameTimeDiagnosticsPlugin,
     prelude::*,
     window::{PresentMode, WindowMode, WindowTheme},
 };
-use space_editor::{simple_editor_setup, SpaceEditorPlugin};
+//use space_editor::{simple_editor_setup, SpaceEditorPlugin};
 fn main() {
     App::new()
-        .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "I am a window!".into(),
-                    decorations: false,
-                    mode: WindowMode::Windowed,
-                    present_mode: PresentMode::AutoVsync,
-                    // Tells wasm to resize the window according to the available canvas
-                    fit_canvas_to_parent: true,
-                    // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
-                    prevent_default_event_handling: false,
-                    window_theme: Some(WindowTheme::Dark),
-                    ..default()
-                }),
-                ..default()
-            }),
-            //            LogDiagnosticsPlugin::default(),
-            FrameTimeDiagnosticsPlugin,
-            SpaceEditorPlugin::default(),
-        ))
+        .add_plugins(
+            (
+                DefaultPlugins
+                    .set(WindowPlugin {
+                        primary_window: Some(Window {
+                            title: "I am a window!".into(),
+                            decorations: false,
+                            mode: WindowMode::Windowed,
+                            present_mode: PresentMode::AutoVsync,
+                            // Tells wasm to resize the window according to the available canvas
+                            fit_canvas_to_parent: true,
+                            // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                            prevent_default_event_handling: false,
+                            window_theme: Some(WindowTheme::Dark),
+                            ..default()
+                        }),
+                        ..default()
+                    })
+                    .set(ImagePlugin::default_nearest()),
+                //            LogDiagnosticsPlugin::default(),
+                FrameTimeDiagnosticsPlugin,
+            ), //            SpaceEditorPlugin::default(),
+        )
         .add_systems(Startup, setup)
-        .add_systems(Startup, simple_editor_setup)
+        //        .add_systems(Startup, simple_editor_setup)
         .run();
 }
 
