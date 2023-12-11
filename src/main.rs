239f0c2612513ccf828c6cde4ba5cc102547c427
pillowtrucker <githubino@janitor.chat>
2023-12-11 21:23:43 +0100

    I can get most of the fps counter from the cheatbook working but I'm probably going to need egui for other gui stuff, since the built-in seems to be limited to the 2d camera viewport..

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,13 @@
 )]
 
 use bevy::{
-    diagnostic::FrameTimeDiagnosticsPlugin,
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
     prelude::*,
     render::camera::Viewport,
     window::{PresentMode, WindowMode, WindowResized, WindowResolution, WindowTheme},
 };
 use bevy_ecs_ldtk::{
-    app::LdtkEntityAppExt,
-    ldtk::{LayerDefinition, LayerInstance, TileInstance},
-    GridCoords, LayerMetadata, LdtkEntity, LdtkPlugin, LdtkWorldBundle, LevelSelection,
+    app::LdtkEntityAppExt, GridCoords, LdtkEntity, LdtkPlugin, LdtkWorldBundle, LevelSelection,
 };
 //use space_editor::{simple_editor_setup, SpaceEditorPlugin};
 fn main() {
@@ -46,8 +44,17 @@ fn main() {
                 LdtkPlugin,
             ), //            SpaceEditorPlugin::default(),
         )
-        .add_systems(Startup, (setup))
-        .add_systems(Update, (set_camera_viewports, camera_follow, hint_color))
+        .add_systems(Startup, (setup, setup_fps_counter))
+        .add_systems(
+            Update,
+            (
+                set_camera_viewports,
+                camera_follow,
+                hint_color,
+                fps_text_update_system,
+                fps_counter_showhide,
+            ),
+        )
         //        .add_systems(Startup, simple_editor_setup)
         .insert_resource(LevelSelection::Identifier("Level_0".to_owned()))
         .register_ldtk_entity::<PlayerBundle>("ThePlayer")
@@ -155,8 +162,10 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ..default()
     });
 }
+
 fn set_camera_viewports(
     windows: Query<&Window>,
+    //    mut fpsdings: Query<&mut Style, With<FpsRoot>>,
     mut resize_events: EventReader<WindowResized>,
     mut window_move_events: EventReader<WindowMoved>,
     mut camera2d: Query<&mut Camera, (With<MyCamera2D>, Without<MyCamera3D>)>,
@@ -169,6 +178,7 @@ fn set_camera_viewports(
     for resize_event in resize_events.read() {
         let window = windows.get(resize_event.window).unwrap();
         let mut camera2d = camera2d.single_mut();
+
         camera2d.viewport = Some(Viewport {
             physical_position: UVec2::new(0, 0),
             physical_size: UVec2 {
@@ -177,6 +187,12 @@ fn set_camera_viewports(
             },
             ..default()
         });
+        /*
+        let mut fps = fpsdings.single_mut();
+        fps.left = Val::Percent(1.);
+        fps.top = Val::Percent(1.);
+        fps.position_type = PositionType::Absolute;
+        */
     }
     for move_event in window_move_events.read() {
         let window = windows.get(move_event.entity).unwrap();
@@ -189,6 +205,12 @@ fn set_camera_viewports(
             },
             ..default()
         });
+        /*
+        let mut fps = fpsdings.single_mut();
+        fps.left = Val::Percent(1.);
+        fps.top = Val::Percent(1.);
+        fps.position_type = PositionType::Absolute;
+        */
     }
 }
 
@@ -234,4 +256,136 @@ fn hint_color(
         other_sprite.color = Color::BLACK;
     }
     */
+}
+/*
+fn unfuck_fps_position(mut fpsdings: Query<&mut Style, With<FpsRoot>>) {
+    let mut fps = fpsdings.single_mut();
+    fps.right = Val::Percent(1.);
+    fps.top = Val::Percent(1.);
+    fps.position_type = PositionType::Absolute;
+}
+*/
+/// Marker to find the container entity so we can show/hide the FPS counter
+#[derive(Component)]
+struct FpsRoot;
+
+/// Marker to find the text entity so we can update it
+#[derive(Component)]
+struct FpsText;
+
+fn setup_fps_counter(mut commands: Commands) {
+    // create our UI root node
+    // this is the wrapper/container for the text
+    let root = commands
+        .spawn((
+            FpsRoot,
+            NodeBundle {
+                // give it a dark background for readability
+                background_color: BackgroundColor(Color::BLACK.with_a(0.5)),
+                // make it "always on top" by setting the Z index to maximum
+                // we want it to be displayed over all other UI
+                z_index: ZIndex::Global(i32::MAX),
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    // position it at the top-right corner
+                    // 1% away from the top window edge
+                    right: Val::Auto,
+                    top: Val::Percent(0.5),
+                    // set bottom/left to Auto, so it can be
+                    // automatically sized depending on the text
+                    bottom: Val::Auto,
+                    left: Val::Percent(0.5),
+                    // give it some padding for readability
+                    padding: UiRect::all(Val::Px(4.0)),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        ))
+        .id();
+    // create our text
+    let text_fps = commands
+        .spawn((
+            FpsText,
+            TextBundle {
+                // use two sections, so it is easy to update just the number
+                text: Text::from_sections([
+                    TextSection {
+                        value: "FPS: ".into(),
+                        style: TextStyle {
+                            font_size: 16.0,
+                            color: Color::WHITE,
+                            // if you want to use your game's font asset,
+                            // uncomment this and provide the handle:
+                            // font: my_font_handle
+                            ..default()
+                        },
+                    },
+                    TextSection {
+                        value: " N/A".into(),
+                        style: TextStyle {
+                            font_size: 16.0,
+                            color: Color::WHITE,
+                            // if you want to use your game's font asset,
+                            // uncomment this and provide the handle:
+                            // font: my_font_handle
+                            ..default()
+                        },
+                    },
+                ]),
+                ..Default::default()
+            },
+        ))
+        .id();
+    commands.entity(root).push_children(&[text_fps]);
+}
+
+fn fps_text_update_system(
+    diagnostics: Res<DiagnosticsStore>,
+    mut query: Query<&mut Text, With<FpsText>>,
+) {
+    for mut text in &mut query {
+        // try to get a "smoothed" FPS value from Bevy
+        if let Some(value) = diagnostics
+            .get(FrameTimeDiagnosticsPlugin::FPS)
+            .and_then(|fps| fps.smoothed())
+        {
+            // Format the number as to leave space for 4 digits, just in case,
+            // right-aligned and rounded. This helps readability when the
+            // number changes rapidly.
+            text.sections[1].value = format!("{value:>4.0}");
+
+            // Let's make it extra fancy by changing the color of the
+            // text according to the FPS value:
+            text.sections[1].style.color = if value >= 120.0 {
+                // Above 120 FPS, use green color
+                Color::rgb(0.0, 1.0, 0.0)
+            } else if value >= 60.0 {
+                // Between 60-120 FPS, gradually transition from yellow to green
+                Color::rgb((1.0 - (value - 60.0) / (120.0 - 60.0)) as f32, 1.0, 0.0)
+            } else if value >= 30.0 {
+                // Between 30-60 FPS, gradually transition from red to yellow
+                Color::rgb(1.0, ((value - 30.0) / (60.0 - 30.0)) as f32, 0.0)
+            } else {
+                // Below 30 FPS, use red color
+                Color::rgb(1.0, 0.0, 0.0)
+            }
+        } else {
+            // display "N/A" if we can't get a FPS measurement
+            // add an extra space to preserve alignment
+            text.sections[1].value = " N/A".into();
+            text.sections[1].style.color = Color::WHITE;
+        }
+    }
+}
+
+/// Toggle the FPS counter when pressing F6
+fn fps_counter_showhide(mut q: Query<&mut Visibility, With<FpsRoot>>, kbd: Res<Input<KeyCode>>) {
+    if kbd.just_pressed(KeyCode::F6) {
+        let mut vis = q.single_mut();
+        *vis = match *vis {
+            Visibility::Hidden => Visibility::Visible,
+            _ => Visibility::Hidden,
+        };
+    }
 }
